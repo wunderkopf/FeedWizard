57f56a30639022100736c06c231c1c6848bf6afd
Sasha Kurylenko <sasha@guest4.dacons.internal>
2011-08-31 16:27:36 +0300

    Initial Commit

---
+++ FeedWizard/FeedWizardAppDelegate.h
@@ -0,0 +1,26 @@
+//
+//  FeedWizardAppDelegate.h
+//  FeedWizard
+//
+//  Created by Sasha Kurylenko on 8/31/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface FeedWizardAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+    NSPersistentStoreCoordinator *__persistentStoreCoordinator;
+    NSManagedObjectModel *__managedObjectModel;
+    NSManagedObjectContext *__managedObjectContext;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
