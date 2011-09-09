42d716ea91df7f7f302abeefae99de2971e20ebd
Sasha Kurylenko <sasha@alex_macbook.dacons.internal>
2011-09-09 10:45:35 +0300

    Added FeedController class - not implemented yet

---
+++ FeedWizard/FeedController.h
@@ -0,0 +1,14 @@
+//
+//  FeedController.h
+//  FeedWizard
+//
+//  Created by Sasha Kurylenko on 9/7/11.
+//  Copyright 2011 Wunderkopf. All rights reserved.
+//
+
+// TODO: not implemented yet
+
+@interface FeedController : NSObject
+
+@end
+
---
+++ FeedWizard/FeedController.m
@@ -0,0 +1,23 @@
+//
+//  FeedController.m
+//  FeedWizard
+//
+//  Created by Sasha Kurylenko on 9/7/11.
+//  Copyright 2011 Wunderkopf. All rights reserved.
+//
+
+#import "FeedController.h"
+
+@implementation FeedController
+
+- (id)init
+{
+    self = [super init];
+    if (self) 
+    {
+    }
+    
+    return self;
+}
+
+@end
