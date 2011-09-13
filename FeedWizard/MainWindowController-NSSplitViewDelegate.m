2340cf98d5a291efbf4f1590ccd69478f24d07c8
Sasha Kurylenko <sasha@alex_macbook.dacons.internal>
2011-09-13 15:59:03 +0300

    Added split view delegate implementation file

---
+++ FeedWizard/MainWindowController-NSSplitViewDelegate.m
@@ -0,0 +1,13 @@
+//
+//  MainWindowController-NSSplitViewDelegate.m
+//  FeedWizard
+//
+//  Created by Sasha Kurylenko on 9/13/11.
+//  Copyright 2011 Wunderkopf. All rights reserved.
+//
+
+#import "MainWindowController.h"
+
+@implementation MainWindowController (NSSplitViewDelegate)
+
+@end
