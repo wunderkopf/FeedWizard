785402ae078d22d04097df48dcfa958309d4aeaf
Sasha Kurylenko <sasha@alex_macbook.dacons.internal>
2011-09-14 15:24:30 +0300

    Display summary for empty content

--- FeedWizard/MainWindowController-NSTableViewDelegate.m
+++ FeedWizard/MainWindowController-NSTableViewDelegate.m
@@ -21,29 +21,6 @@
     NSArray *selectedObjects = [_entryArrayController selectedObjects];
     
     Entry *entry = [selectedObjects firstObject];
-    
-    /*NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
-	[request setURL:entry.alternateURL];
-	[request setHTTPMethod:@"GET"];
-	[request setHTTPShouldHandleCookies:NO];
-	[request setCachePolicy:NSURLRequestReloadIgnoringLocalAndRemoteCacheData];
-	[request addValue:kUserAgentValue forHTTPHeaderField:@"User-Agent"];
-	//[request setHTTPBody:[postData dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES]];
-    [[_webView mainFrame] loadRequest:request];*/
-    
-    //[[_webView mainFrame] loadHTMLString:entry.content baseURL:entry.baseURL];
-    
-    /*GGReadability *read = [[GGReadability alloc] initWithURL:entry.alternateURL completionHandler:^(NSString *string) {
-        //[[_webView mainFrame] loadHTMLString:string baseURL:entry.baseURL];
-        NSString *filePath = [[NSBundle mainBundle] pathForResource:@"article" ofType:@"html"];
-        NSData *articleData = [NSData dataWithContentsOfFile:filePath];
-        NSString *atricleText = [[NSString alloc] initWithData:articleData encoding:NSStringEncodingConversionAllowLossy];
-        atricleText = [atricleText stringByReplacingOccurrencesOfString:@"[CONTENT]" withString:string];
-        [[_webView mainFrame] loadHTMLString:atricleText baseURL:entry.baseURL];
-    } errorHandler:^(NSError * error) {
-        NSLog(@"Something wrong");
-    }];
-    [read render];*/
 
     NSString *articleText = [NSString string];
     if ([entry.titleForDisplay length] > 0)
@@ -56,7 +33,13 @@
     if ([entry.content length] > 0)
         articleText = [articleText stringByReplacingOccurrencesOfString:@"[CONTENT]" withString:entry.content];
     else
-        articleText = [articleText stringByReplacingOccurrencesOfString:@"[CONTENT]" withString:@""];
+    {
+        //summary
+        if ([entry.summary length] > 0)
+            articleText = [articleText stringByReplacingOccurrencesOfString:@"[CONTENT]" withString:entry.summary];
+        else
+            articleText = [articleText stringByReplacingOccurrencesOfString:@"[CONTENT]" withString:@"No content"];
+    }
     
     [[_webView mainFrame] loadHTMLString:articleText baseURL:entry.baseURL];
 
