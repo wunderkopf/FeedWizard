92e35463d0fd4b4139b8e9a44c409f8b5b1f24f1
Sasha Kurylenko <sasha@alex_macbook.dacons.internal>
2011-09-21 14:37:03 +0300

    Added min size controlling for split

--- FeedWizard/MainWindowController-NSSplitViewDelegate.m
+++ FeedWizard/MainWindowController-NSSplitViewDelegate.m
@@ -10,4 +10,19 @@
 
 @implementation MainWindowController (NSSplitViewDelegate)
 
+- (BOOL)splitView:(NSSplitView *)splitView canCollapseSubview:(NSView *)subview 
+{
+    return NO;
+}
+
+- (BOOL)splitView:(NSSplitView *)splitView shouldCollapseSubview:(NSView *)subview forDoubleClickOnDividerAtIndex:(NSInteger)dividerIndex
+{    
+    return NO;
+}
+
+- (CGFloat)splitView:(NSSplitView *)splitView constrainMinCoordinate:(CGFloat)proposedMinimumPosition ofSubviewAt:(NSInteger)dividerIndex
+{
+    return proposedMinimumPosition + 100;
+}
+
 @end
